5b035fb975e44e1238d3fce9afed7acfad17fed3
Yang Yunseo <ytmm48@gmail.com>
2025-07-16 19:22:29 +0900

    answer the questions

--- day05_homework.R
+++ day05_homework.R
@@ -1,12 +1,14 @@
 # DAY 5 HOMEWORK
 #
-# Name:
+# Name: Yang Yunseo
 
 # NOTE: In the text below, backticks ``
 # are used to indicate code. For example, `library(tidyverse)`
 
 # Before doing anything else, load the packages you need for this assignment:
 # `tidyverse` and `nycflights13`
+library(tidyverse)
+library(nycflights13)
 
 # Q1. The `nycflights13` package includes a dataframe called `planes` that
 # contains data for each airplane such as manufacturer, model, etc.
@@ -15,17 +17,22 @@
 # `flights`. Save the result as answer_1
 #
 # HINT: the `tailnum` column uniquely identifies each airplane.
+answer_1 <- flights |>
+  left_join(planes, join_by(tailnum))
 
 # Q2. Use answer_1 to count the number of flights made by each manufacturer,
 # then sort the results so the manufacturer with the most flights is at the top.
 # Save the result as answer_2
 #
 # HINT: use `count()`
+answer_2 <- answer_1 |>
+  count(manufacturer, sort = TRUE)
 
 # Q3. Does every flight in the `flights` dataframe have a corresponding
 # airplane in `planes`?
 #
 # Save your answer as a logical vector (either TRUE or FALSE) to answer_3
+answer_3 <- FALSE
 
 # Q4. The `nycflights13` package includes a dataframe called `airlines` that
 # includes the name of each airline and its corresponding carrier code.
@@ -33,6 +40,8 @@
 #
 # Join the `airlines` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_4
+answer_4 <- flights |>
+  left_join(airlines)
 
 # Q5. Use answer_4 to find the average arrival delay for each airline.
 # Exclude missing values from your calculation.
@@ -43,4 +52,7 @@
 #
 # HINT: when you use functions like `mean()`, `max()`, or `min()`, use the
 # option `na.rm = TRUE` to exclude missing values from the calculation
-
+answer_5 <- answer_4 |>
+  group_by(carrier) |>
+  summarize(mean_delay = mean(arr_delay, na.rm = TRUE)) |>
+  arrange(desc(mean_delay))
